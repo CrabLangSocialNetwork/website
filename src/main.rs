6c84f263550278881ebd2ebffc77081007cd1595
RIC217 <melericcardo9400@gmail.com>
2023-06-20 16:07:52 +0200

    Added dependencies and template code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+struct Post {
+    content: String
+}
+
+#[derive(Clone, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home
+}
+
+#[function_component]
+fn Home() -> Html {
+    let posts = use_state(|| Vec::<Post>::new());
+
+    html! {
+
+    }
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {<h1>{"Home"}</h1>}
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
